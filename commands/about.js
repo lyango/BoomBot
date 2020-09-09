b5f0290a085854bb885256ab008330dd65773804
Leon Yang <ley015@ucsd.edu>
2020-09-09 04:16:17 -0700

    update about cmd

--- commands/about.js
+++ commands/about.js
@@ -7,9 +7,9 @@ module.exports = {
         const aboutEmbed = new Discord.MessageEmbed()
         .setColor("#fc0303")
         .setTitle("BOOM BOT")
-        .setDescription("Boom Bot is a Valorant Information Bot made for Discord. It actively takes data/information from the official" + 
+        .setDescription("Boom Bot is a Valorant information bot made for Discord. It actively takes data/information from the official" + 
         " Valorant website, and will concurrently update based off Riot Games automatically.")
-        .setFooter("Made by Leon / Lyang#5206 on Discord")
+        .setFooter("Made by Leon / lyango on GitHub / Lyang#5206 on Discord")
         .setThumbnail("https://cdn.discordapp.com/avatars/752694272414842941/a73c01ee414e59aab58f0935fbae254b.png?size=256");
         //.setThumbnail(client.user.displayAvatarURL());
 
